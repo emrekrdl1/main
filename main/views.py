dceb96b93092998704364ded12ed5f94c70626e3
emre <emirk314314@gmail.com>
2018-03-01 20:37:26 +0300

    Django uygulamasında ilk commit'im

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import *
+from django.contrib.auth import *
+from django.contrib.auth.decorators import *
+from django.http import *
+
 def post_list(request):
     return render(request, 'post_list.html', {})
